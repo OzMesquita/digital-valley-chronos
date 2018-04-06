d69fe39f2a00aa290ff1b459d1e7b44c091f124e
Alexandre Mendonça <franciscoalexandre.m.c@gmail.com>
2018-04-06 13:38:16 -0300

    Atributo minicurso adicionado a classe de modelo Palestrante.java

--- src/main/java/Model/Palestrante.java
+++ src/main/java/Model/Palestrante.java
@@ -3,5 +3,14 @@ package Model;
 import model.Pessoa;
 
 public class Palestrante extends Pessoa{
+	private String miniCurriculo;
+
+	public String getMiniCurriculo() {
+		return miniCurriculo;
+	}
+
+	public void setMiniCurriculo(String miniCurriculo) {
+		this.miniCurriculo = miniCurriculo;
+	}
 	
 }
