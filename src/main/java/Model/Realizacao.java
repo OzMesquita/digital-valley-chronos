d9210b1ac1c4e3ab764d64ce26fcb3e8bbad67f4
Isaias Ferreira <isaiasfs1997@hotmail.com>
2018-04-17 12:18:00 -0300

    Implementação do padrão na classe Realizacao.java.
    A sobrescrita do metodo compareTo() é feita para que seja comparado as datas inicias de duas realizações.

--- src/main/java/Model/Realizacao.java
+++ src/main/java/Model/Realizacao.java
@@ -2,20 +2,20 @@ package Model;
 
 import java.time.LocalDateTime;
 
-public class Realizacao {
+public class Realizacao implements Comparable<Realizacao> {
 	private LocalDateTime horaInicio;
 	private LocalDateTime horaFinal;
 
-	
+
 	public Realizacao(LocalDateTime horaInicio, LocalDateTime horaFinal) {
 		setHoraInicio(horaInicio);
 		setHoraFinal(horaFinal);
 	}
-	
+
 	public LocalDateTime getHoraInicio() {
 		return horaInicio;
 	}
-	
+
 	public void setHoraInicio(LocalDateTime horaInicio) {
 		if(horaInicio == null)
 			throw new IllegalArgumentException("Erro: o campo da hora inicial não pode estar vazio.");
@@ -24,11 +24,11 @@ public class Realizacao {
 		else
 			this.horaInicio = horaInicio;
 	}
-	
+
 	public LocalDateTime getHoraFinal() {
 		return horaFinal;
 	}
-	
+
 	public void setHoraFinal(LocalDateTime horaFinal) {
 		if(horaFinal == null)
 			throw new IllegalArgumentException("Erro: o campo hora final não pode estar vazio.");
@@ -39,5 +39,14 @@ public class Realizacao {
 		else
 			this.horaFinal = horaFinal;
 	}
-	
+
+	@Override
+	public int compareTo(Realizacao outraRealizacao) {
+		if (this.horaInicio.isAfter(outraRealizacao.getHoraInicio()))
+			return 1;
+		else if (this.horaInicio.isBefore(outraRealizacao.getHoraInicio()))
+			return -1;
+		else
+			return 0;
+	}
 }
