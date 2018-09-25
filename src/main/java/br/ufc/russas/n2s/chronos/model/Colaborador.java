4b7ca250e6c0ac5666b887fc6f24a178b33005b8
Alexandre Mendonça <franciscoalexandre.m.c@gmail.com>
2018-09-25 15:36:56 -0300

    Finalização da classe modelo para Colaborador

--- src/main/java/br/ufc/russas/n2s/chronos/model/Colaborador.java
+++ src/main/java/br/ufc/russas/n2s/chronos/model/Colaborador.java
@@ -1,11 +1,64 @@
 package br.ufc.russas.n2s.chronos.model;
 
-public class Colaborador {
+public class Colaborador implements Comparable<Colaborador>{
 
 	private long CodColaborador;
+	private String nome;
+	private String funcao;
 	
+	public Colaborador( String nome, String funcao) {
+		this.nome = nome;
+		this.funcao = funcao;
+	}
+	
+	public Colaborador() {
+
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		if (!isEmpty(nome))
+			this.nome = nome;
+		else
+			throw new IllegalArgumentException("Erro: o campo nome não pode estar vazio");
+	}
+
+
+	public String getFuncao() {
+		return funcao;
+	}
+
+
+	public void setFuncao(String funcao) {
+		if (!isEmpty(funcao))
+			this.funcao = funcao;
+		else
+			throw new IllegalArgumentException("Erro: o campo função não pode estar vazio");
+	}
+
+
+	public void setCodColaborador(long codColaborador) {
+		CodColaborador = codColaborador;
+	}
+
+
 	public long getCodColaborador() {
 		return this.CodColaborador;
 	}
 
+
+	@Override
+	public int compareTo(Colaborador o) {
+		return this.getNome().compareTo(o.getNome());
+	}
+
+	public static boolean isEmpty(String string) {
+		if (string == null || string.equals(""))
+			return true;
+		return false;
+	}
 }
