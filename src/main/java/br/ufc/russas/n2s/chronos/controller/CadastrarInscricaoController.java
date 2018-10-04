1b1fdaaf031d753c2ae300bdf7490f6641954b8a
Jhean Marllos Gonçalves Sombra <jheanmarllos1@gmail.com>
2018-10-04 14:56:23 -0300

    Controller de cadastro de inscrição
    Controller completo feito | Mapeamento do controller no dispacherServlet.xml | Mapeamento da classe de modelo da inscrição no aplicationContext.xml

---
+++ src/main/java/br/ufc/russas/n2s/chronos/controller/CadastrarInscricaoController.java
@@ -0,0 +1,76 @@
+package br.ufc.russas.n2s.chronos.controller;
+
+import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import br.ufc.russas.n2s.chronos.beans.AtividadeBeans;
+import br.ufc.russas.n2s.chronos.beans.InscricaoAtividadeBeans;
+import br.ufc.russas.n2s.chronos.beans.UsuarioBeans;
+import br.ufc.russas.n2s.chronos.service.AtividadeServiceIfc;
+import br.ufc.russas.n2s.chronos.service.UsuarioServiceIfc;
+
+@Controller("cadastrarInscricaoController")
+@RequestMapping("/cadastrarInscricao")
+public class CadastrarInscricaoController {
+	private AtividadeServiceIfc atividadeServiceIfc;
+	private UsuarioServiceIfc usuarioServiceIfc;
+
+	public AtividadeServiceIfc getAtividadeService() {
+		return atividadeServiceIfc;
+	}
+
+	@Autowired(required = true)
+	public void setAtividadeServiceIfc(@Qualifier("atividadeServiceIfc") AtividadeServiceIfc atividadeService) {
+		this.atividadeServiceIfc = atividadeService;
+
+	}
+
+	public UsuarioServiceIfc getUsuarioServiceIfc() {
+		return usuarioServiceIfc;
+	}
+
+	@Autowired(required = true)
+	public void setUsuarioServiceIfc(@Qualifier("usuarioServiceIfc") UsuarioServiceIfc usuarioServiceIfc) {
+		this.usuarioServiceIfc = usuarioServiceIfc;
+	}
+
+	@RequestMapping(value = "/{codAtividade}", method = RequestMethod.GET)
+	public String getCadastro(@PathVariable long codAtividade, Model model, HttpServletRequest request) {
+		return "cadastrar-inscricao";
+	}
+
+	@RequestMapping(value = "/cadastraInscricao/{codAtividade}", method = RequestMethod.POST)
+	public String adiciona(@PathVariable long codAtividade,
+			@ModelAttribute("inscricao") @Valid InscricaoAtividadeBeans inscricao, BindingResult result, Model model,
+			HttpServletResponse response, HttpServletRequest request) throws IOException, IllegalAccessException {
+		HttpSession session = request.getSession();
+		UsuarioBeans usuario = (UsuarioBeans) session.getAttribute("usuarioChronos");
+
+		try {
+			this.atividadeServiceIfc.setUsuario(usuario);
+			AtividadeBeans atividade = this.getAtividadeService().getAtividade(codAtividade);
+			//Remover esse comentário quando alterar o model e bean de atividade
+			// atividade.getInscritos.add(inscricao);
+			atividade = this.getAtividadeService().atualizaAtividade(atividade);
+			session.setAttribute("mensagem", "Inscrição realizada com sucesso!");
+			session.setAttribute("status", "success");
+			return ("redirect:/inscricao/" + codAtividade);
+		} catch (IllegalAccessException e) {
+			session.setAttribute("mensagem", e.getMessage());
+			session.setAttribute("status", "danger");
+			return ("redirect:/inscricao/" + codAtividade);
+		}
+	}
+}
