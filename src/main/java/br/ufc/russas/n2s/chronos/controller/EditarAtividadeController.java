22656599524cc52bcdcf03e42adda3de359e7b99
Alexandre Mendonça <franciscoalexandre.m.c@gmail.com>
2018-10-11 15:01:40 -0300

    Atualização do Core.N2S

--- src/main/java/br/ufc/russas/n2s/chronos/controller/EditarAtividadeController.java
+++ src/main/java/br/ufc/russas/n2s/chronos/controller/EditarAtividadeController.java
@@ -1,7 +1,5 @@
 package br.ufc.russas.n2s.chronos.controller;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 
@@ -12,13 +10,11 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 
