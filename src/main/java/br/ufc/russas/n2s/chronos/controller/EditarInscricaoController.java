92a7f3e8fda0b43e29d71e0dc693d79bf853221f
Alexandre Mendonça <franciscoalexandre.m.c@gmail.com>
2018-10-23 15:06:36 -0300

    Exclusão de metodos inuteis

--- src/main/java/br/ufc/russas/n2s/chronos/controller/EditarInscricaoController.java
+++ src/main/java/br/ufc/russas/n2s/chronos/controller/EditarInscricaoController.java
@@ -1,6 +1,5 @@
 package br.ufc.russas.n2s.chronos.controller;
 
-import java.io.IOException;
 import java.util.Iterator;
 
 import javax.servlet.http.HttpServletRequest;
@@ -20,8 +19,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import br.ufc.russas.n2s.chronos.beans.AtividadeBeans;
 import br.ufc.russas.n2s.chronos.beans.UsuarioBeans;
-import br.ufc.russas.n2s.chronos.model.Atividade;
-import br.ufc.russas.n2s.chronos.model.UsuarioChronos;
 import br.ufc.russas.n2s.chronos.service.AtividadeServiceIfc;
 import br.ufc.russas.n2s.chronos.service.UsuarioServiceIfc;
 
@@ -54,40 +51,6 @@ public class EditarInscricaoController {
 	public String getCadastro(@PathVariable long codAtividade, @PathVariable long codInscricao, Model model,
 			HttpServletRequest request) {
 		return "inscricao";
-	}
-	//Funciona quando o model e o Bean de atividade for alterado para refletir a inscrição
-	@RequestMapping(value = "/{codAtividade}&{codInscricao}/editar", method = RequestMethod.POST)
-	public String editaInscricao(@PathVariable long codAtividade, @PathVariable long codInscricao,
-			@ModelAttribute("participante") @Valid UsuarioChronos participante, @ModelAttribute("atividade") @Valid Atividade atividade, BindingResult result, Model model,
-			HttpServletResponse response, HttpServletRequest request) throws IOException, IllegalAccessException {
-		HttpSession session = request.getSession();
-		UsuarioBeans usuario = (UsuarioBeans) session.getAttribute("usuarioChronos");
-		this.atividadeServiceIfc.setUsuario(usuario);
-		AtividadeBeans atividadeBeans = this.getAtividadeService().getAtividade(codAtividade);
-		if (atividadeBeans != null) {
-			try {
-//				for (Iterator<InscricaoAtividadeBeans> iterator = atividadeBeans.getInscricoes().iterator(); iterator
-//						.hasNext();) {
-//					InscricaoAtividadeBeans inscricaoAUX = iterator.next();
-//					if (inscricaoAUX.getCodInscricao() == codInscricao) {
-//						inscricaoAUX.setParticipante(participante);
-//						inscricaoAUX.setAtividade(atividade);
-//						break;
-//					}
-//				}
-				atividadeBeans = this.getAtividadeService().atualizaAtividade(atividadeBeans);
-				session.setAttribute("mensagem", "Inscricao editada com sucesso!");
-				session.setAttribute("status", "success");
-				return ("redirect:/inscricoes/" + codAtividade);
-			}
-
-			catch (IllegalArgumentException e) {
-				session.setAttribute("mensagem", e.getMessage());
-				session.setAttribute("status", "danger");
-				return ("redirect:/inscricoes/" + codAtividade);
-			}
-		}
-		return null;
 	}
 	
 	@RequestMapping(value = "/{codAtividade}&{codInscricao}/remover", method = RequestMethod.POST)
