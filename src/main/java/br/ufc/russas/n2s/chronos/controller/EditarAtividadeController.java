de870577b878897598fba374790f28531f7dfd56
Jhean Marllos Gonçalves Sombra <jheanmarllos1@gmail.com>
2018-11-16 16:39:56 -0300

    Atualização do método de remoção para remover todas as inscrições da atividade antes de fazer a remoção da mesma do sistema.

--- src/main/java/br/ufc/russas/n2s/chronos/controller/EditarAtividadeController.java
+++ src/main/java/br/ufc/russas/n2s/chronos/controller/EditarAtividadeController.java
@@ -37,152 +37,169 @@ public class EditarAtividadeController {
 		return atividadeServiceIfc;
 	}
 
-	@Autowired(required=true)
+	@Autowired(required = true)
 	public void setAtividadeServiceIfc(@Qualifier("atividadeServiceIfc") AtividadeServiceIfc atividadeService) {
 		this.atividadeServiceIfc = atividadeService;
 
 	}
+
 	public UsuarioServiceIfc getUsuarioService() {
 		return usuarioServiceIfc;
 	}
+
 	@Autowired(required = true)
-	public void setUsuarioServiceIfc(@Qualifier("usuarioServiceIfc")UsuarioServiceIfc usuarioServiceIfc) {
+	public void setUsuarioServiceIfc(@Qualifier("usuarioServiceIfc") UsuarioServiceIfc usuarioServiceIfc) {
 		this.usuarioServiceIfc = usuarioServiceIfc;
 	}
-		
-	@RequestMapping(value="/{codAtividade}",method = RequestMethod.GET)
-	public String getIndex(@PathVariable long codAtividade, Model model, HttpServletRequest request ) {
-		 AtividadeBeans atividade = atividadeServiceIfc.getAtividade(codAtividade);
-        request.getSession().setAttribute("atividade", atividade);
-        if(atividade.getPai()!=null) {
-        	request.getSession().setAttribute("colaboradoresselecionados", atividade.getColaboradores());
-        	List<Colaborador> colaboradores = atividade.getPai().getColaboradores();
-        	for (Iterator<ColaboradorBeans> iterator = atividade.getColaboradores().iterator(); iterator.hasNext();) {
-        		long codcolaborador = iterator.next().getCodColaborador();
-        		for (Iterator<Colaborador> iterator2 = colaboradores.iterator(); iterator2.hasNext();)
-					if(codcolaborador==iterator2.next().getCodColaborador())
+
+	@RequestMapping(value = "/{codAtividade}", method = RequestMethod.GET)
+	public String getIndex(@PathVariable long codAtividade, Model model, HttpServletRequest request) {
+		AtividadeBeans atividade = atividadeServiceIfc.getAtividade(codAtividade);
+		request.getSession().setAttribute("atividade", atividade);
+		if (atividade.getPai() != null) {
+			request.getSession().setAttribute("colaboradoresselecionados", atividade.getColaboradores());
+			List<Colaborador> colaboradores = atividade.getPai().getColaboradores();
+			for (Iterator<ColaboradorBeans> iterator = atividade.getColaboradores().iterator(); iterator.hasNext();) {
+				long codcolaborador = iterator.next().getCodColaborador();
+				for (Iterator<Colaborador> iterator2 = colaboradores.iterator(); iterator2.hasNext();)
+					if (codcolaborador == iterator2.next().getCodColaborador())
 						iterator2.remove();
-        	}
+			}
 			request.getSession().setAttribute("colaboradores", colaboradores);
-        }
+		}
 		return "editar-atividade";
 	}
-	
-	@RequestMapping(value="/{codAtividade}",method = RequestMethod.POST)
-	public String atualizaAtividade(@PathVariable long codAtividade, @ModelAttribute("atividade") @Valid AtividadeBeans atividade, BindingResult result, Model model, HttpServletResponse reponse, HttpServletRequest request ) throws  IllegalAccessException{ 
-		 AtividadeBeans atividadeBeans = atividadeServiceIfc.getAtividade(codAtividade);
-		 HttpSession session = request.getSession();
-		 String[] colaboradoresDisponiveis = request.getParameterValues("colaboradorInput1");
-		 String[] colaboradoresSelecionados = request.getParameterValues("colaboradorInput2");
-		 
-		 {
-			 try {
-				 atividadeBeans.setNome(atividade.getNome());
-				 atividadeBeans.setDescricao(atividade.getDescricao());
-				 atividadeBeans.setSigla(atividade.getSigla());
-				 atividadeBeans.setTipoAtividade(atividade.getTipoAtividade());
-				 atividadeBeans.setLocal(atividade.getLocal());
-				 atividadeBeans.setPreRequisitos(atividade.getPreRequisitos());
-				 atividadeBeans.setTotalVagas(atividade.getTotalVagas());
-				 atividadeBeans.setTotalVagasComunidade(atividade.getTotalVagasComunidade());
-				 atividadeBeans.setTipoPagamento(atividade.getTipoPagamento());
-				 atividadeBeans.setOrganizadores(atividade.getOrganizadores());
-				 
-				 if(atividadeBeans.getPai()!=null) {
-					 if(colaboradoresSelecionados!=null)
-						 for (Colaborador colaboradorDisp : atividadeBeans.getPai().getColaboradores()) {
-							for(String codcolaborador : colaboradoresSelecionados)
-								if(colaboradorDisp.getCodColaborador()==Long.parseLong(codcolaborador)) {
-									for (Iterator<ColaboradorBeans> iterator = atividadeBeans.getColaboradores().iterator(); iterator.hasNext();)
-										if(iterator.next().getCodColaborador()==colaboradorDisp.getCodColaborador())
+
+	@RequestMapping(value = "/{codAtividade}", method = RequestMethod.POST)
+	public String atualizaAtividade(@PathVariable long codAtividade,
+			@ModelAttribute("atividade") @Valid AtividadeBeans atividade, BindingResult result, Model model,
+			HttpServletResponse reponse, HttpServletRequest request) throws IllegalAccessException {
+		AtividadeBeans atividadeBeans = atividadeServiceIfc.getAtividade(codAtividade);
+		HttpSession session = request.getSession();
+		String[] colaboradoresDisponiveis = request.getParameterValues("colaboradorInput1");
+		String[] colaboradoresSelecionados = request.getParameterValues("colaboradorInput2");
+
+		{
+			try {
+				atividadeBeans.setNome(atividade.getNome());
+				atividadeBeans.setDescricao(atividade.getDescricao());
+				atividadeBeans.setSigla(atividade.getSigla());
+				atividadeBeans.setTipoAtividade(atividade.getTipoAtividade());
+				atividadeBeans.setLocal(atividade.getLocal());
+				atividadeBeans.setPreRequisitos(atividade.getPreRequisitos());
+				atividadeBeans.setTotalVagas(atividade.getTotalVagas());
+				atividadeBeans.setTotalVagasComunidade(atividade.getTotalVagasComunidade());
+				atividadeBeans.setTipoPagamento(atividade.getTipoPagamento());
+				atividadeBeans.setOrganizadores(atividade.getOrganizadores());
+
+				if (atividadeBeans.getPai() != null) {
+					if (colaboradoresSelecionados != null)
+						for (Colaborador colaboradorDisp : atividadeBeans.getPai().getColaboradores()) {
+							for (String codcolaborador : colaboradoresSelecionados)
+								if (colaboradorDisp.getCodColaborador() == Long.parseLong(codcolaborador)) {
+									for (Iterator<ColaboradorBeans> iterator = atividadeBeans.getColaboradores()
+											.iterator(); iterator.hasNext();)
+										if (iterator.next().getCodColaborador() == colaboradorDisp.getCodColaborador())
 											iterator.remove();
-									atividadeBeans.getColaboradores().add((ColaboradorBeans) new ColaboradorBeans().toBeans(colaboradorDisp));
+									atividadeBeans.getColaboradores()
+											.add((ColaboradorBeans) new ColaboradorBeans().toBeans(colaboradorDisp));
 								}
-						 }
-					 if(colaboradoresDisponiveis!=null)
-						 for(String codcolaboradorDisponivel : colaboradoresDisponiveis)
-							for (Iterator<ColaboradorBeans> iterator = atividadeBeans.getColaboradores().iterator(); iterator.hasNext();)
-								if(iterator.next().getCodColaborador()==Long.parseLong(codcolaboradorDisponivel))
+						}
+					if (colaboradoresDisponiveis != null)
+						for (String codcolaboradorDisponivel : colaboradoresDisponiveis)
+							for (Iterator<ColaboradorBeans> iterator = atividadeBeans.getColaboradores()
+									.iterator(); iterator.hasNext();)
+								if (iterator.next().getCodColaborador() == Long.parseLong(codcolaboradorDisponivel))
 									iterator.remove();
-				 }
-				 
-				 UsuarioBeans usuario = (UsuarioBeans) session.getAttribute("usuarioChronos");
-				 this.getAtividadeServiceIfc().setUsuario(usuario);
-				 atividadeBeans = this.getAtividadeServiceIfc().atualizaAtividade(atividadeBeans);
-				 session.setAttribute("atividade", atividadeBeans);
-				 session.setAttribute("mensagem", "Atividade atualizada com sucesso!");
-				 session.setAttribute("status", "success");
-				 return ("redirect:../atividades/" + atividade.getCodAtividade());
-			 }catch (IllegalAccessException e) {
-				 model.addAttribute("mensagem", e.getMessage());
-				 model.addAttribute("status", "danger");
-				 return ("redirect:../atividades/" + atividade.getCodAtividade());
+				}
+
+				UsuarioBeans usuario = (UsuarioBeans) session.getAttribute("usuarioChronos");
+				this.getAtividadeServiceIfc().setUsuario(usuario);
+				atividadeBeans = this.getAtividadeServiceIfc().atualizaAtividade(atividadeBeans);
+				session.setAttribute("atividade", atividadeBeans);
+				session.setAttribute("mensagem", "Atividade atualizada com sucesso!");
+				session.setAttribute("status", "success");
+				return ("redirect:../atividades/" + atividade.getCodAtividade());
+			} catch (IllegalAccessException e) {
+				model.addAttribute("mensagem", e.getMessage());
+				model.addAttribute("status", "danger");
+				return ("redirect:../atividades/" + atividade.getCodAtividade());
 			}
-		 } 
+		}
 	}
-	
+
 	@RequestMapping(value = "/divulga/{codAtividade}", method = RequestMethod.GET)
 	public String divulgaAtividade(@PathVariable long codAtividade, HttpServletRequest request) {
-       AtividadeBeans atividade = atividadeServiceIfc.getAtividade(codAtividade);
-        HttpSession session = request.getSession();
-        UsuarioBeans usuario = (UsuarioBeans) session.getAttribute("usuarioChronos");
-        try{
-        	if(!atividade.getRealizacao().isEmpty()) {
-        		atividadeServiceIfc.setUsuario(usuario);
-                atividade.setDivulgada(true);
-                atividade = atividadeServiceIfc.atualizaAtividade(atividade);
-                request.getSession().setAttribute("atividade", atividade);
-                request.getSession().setAttribute("mensagem", "Atividade divulgada com sucesso!");
-				request.getSession().setAttribute("status", "success");	
-                return "redirect:/atividades/" + atividade.getCodAtividade();
-        	}else {
-        		request.getSession().setAttribute("mensagem", "Adicione pelo menos uma realização antes de divulgar a atividade!");
-				request.getSession().setAttribute("status", "danger");        		
-        		return "redirect:/atividades/" + atividade.getCodAtividade();
-        	}
-            
-            
-        }catch(IllegalAccessException e){
-            e.printStackTrace();
-            return "redirect:/atividades/" + atividade.getCodAtividade();
-        }catch(Exception e){
-            e.printStackTrace();
-             return "redirect:/atividades/" + atividade.getCodAtividade();
-        }
-    }
-	
+		AtividadeBeans atividade = atividadeServiceIfc.getAtividade(codAtividade);
+		HttpSession session = request.getSession();
+		UsuarioBeans usuario = (UsuarioBeans) session.getAttribute("usuarioChronos");
+		try {
+			if (!atividade.getRealizacao().isEmpty()) {
+				atividadeServiceIfc.setUsuario(usuario);
+				atividade.setDivulgada(true);
+				atividade = atividadeServiceIfc.atualizaAtividade(atividade);
+				request.getSession().setAttribute("atividade", atividade);
+				request.getSession().setAttribute("mensagem", "Atividade divulgada com sucesso!");
+				request.getSession().setAttribute("status", "success");
+				return "redirect:/atividades/" + atividade.getCodAtividade();
+			} else {
+				request.getSession().setAttribute("mensagem",
+						"Adicione pelo menos uma realização antes de divulgar a atividade!");
+				request.getSession().setAttribute("status", "danger");
+				return "redirect:/atividades/" + atividade.getCodAtividade();
+			}
+
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+			return "redirect:/atividades/" + atividade.getCodAtividade();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "redirect:/atividades/" + atividade.getCodAtividade();
+		}
+	}
+
 	@RequestMapping(value = "/remover/{codAtividade}", method = RequestMethod.GET)
-	public String removeAtividade(@PathVariable long codAtividade, HttpServletRequest request) throws IllegalAccessException {
+	public String removeAtividade(@PathVariable long codAtividade, HttpServletRequest request)
+			throws IllegalAccessException {
 		AtividadeBeans atividade = atividadeServiceIfc.getAtividade(codAtividade);
 		HttpSession session = request.getSession();
 		UsuarioBeans usuario = (UsuarioBeans) session.getAttribute("usuarioChronos");
 		this.atividadeServiceIfc.setUsuario(usuario);
-		if(atividade.getPai() != null) {
+		if (atividade.getPai() != null) {
 			List<Atividade> subAtividadesAUX = atividade.getPai().getSubAtividade();
-			
+
 			for (Iterator<Atividade> iterator = subAtividadesAUX.iterator(); iterator.hasNext();) {
 				Atividade atvAUX = iterator.next();
 				if (atvAUX.getCodAtividade() == codAtividade) {
+					if (!atvAUX.getParticipantes().isEmpty()) {
+						atvAUX.getParticipantes().clear();
+					}
 					iterator.remove();
-					
+
 					atividade = this.getAtividadeServiceIfc().atualizaAtividade(atividade);
-					
+
 					AtividadeBeans atividadeAUX = new AtividadeBeans();
 					atividadeAUX.toBeans(atvAUX);
 					try {
 						atividadeServiceIfc.removeAtividade(atividadeAUX);
 						request.getSession().setAttribute("mensagem", "Atividade removida com sucesso!");
-						request.getSession().setAttribute("status", "success");		
+						request.getSession().setAttribute("status", "success");
 						return "redirect:/inicio";
 					} catch (Exception e) {
 						request.getSession().setAttribute("mensagem", "Erro ao remvoer SubAtividade!");
-						request.getSession().setAttribute("status", "danger");	
+						request.getSession().setAttribute("status", "danger");
 						return "redirect:/inicio";
 					}
 				}
 			}
-		}else {
+		} else {
 			try {
+				if (!atividade.getSubAtividade().isEmpty()) {
+					for (AtividadeBeans atvAUX : atividade.getSubAtividade()) {
+						if (!atvAUX.getParticipantes().isEmpty())
+							atvAUX.getParticipantes().clear();
+					}
+				}
 				atividadeServiceIfc.removeAtividade(atividade);
 				request.getSession().setAttribute("mensagem", "Atividade removida com sucesso!");
 				request.getSession().setAttribute("status", "success");
